28daa3b3723993938addb73e0cfd67b7620dadc0
ace <azatibrakov@gmail.com>
2017-08-09 13:13:56 +0700

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import (setup,
 
 setup(name='ethereum_helpers',
       packages=find_packages(),
-      version='0.0.3',
+      version='0.0.4',
       description='Helper functions for working with ethereum network.',
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
